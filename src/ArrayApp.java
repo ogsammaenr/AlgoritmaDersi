3a347291c257338579c5bd44385d6c9b32522e6e
ogsammaenr <osmangamer152@gmail.com>
2025-02-26 01:32:08 +0300

    deneme

--- src/ArrayApp.java
+++ src/ArrayApp.java
@@ -24,5 +24,86 @@ public class ArrayApp {
         return array;
     }
 
+    public static int dizi_toplami (int[] array) {
+        int sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        return sum;
+    }
+    public static int dizi_ortalamasi (int[] array) {
+        int sum = dizi_toplami(array);
+        return (sum / array.length);
+    }
+    public static int dizi_en_buyuk (int[] array){
+        int en_buyuk = 0;
+        for (int i : array) {
+            if (i > en_buyuk) {
+                en_buyuk = i;
+            }
+        }
+        return en_buyuk;
+    }
+    public static int dizi_en_kucuk (int[] array){
+        int en_kucuk = array[0];
+        for (int i : array) {
+            if (i < en_kucuk) {
+                en_kucuk = i;
+            }
+        }
+        return en_kucuk;
+    }
+    public static int[] matris_satir_en_buyuk (int[][] array){
+        int[] en_buyuk = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] > en_buyuk[i]) {
+                    en_buyuk[i] = array[i][j];
+                }
+            }
+        }
+        return en_buyuk;
+    }
+    public static int[] matris_sutun_en_buyuk (int[][] array){
+        int[] en_buyuk = new int[array[0].length];
+        for (int i = 0; i < array[0].length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[j][i] > en_buyuk[i]) {
+                    en_buyuk[i] = array[j][i];
+                }
+            }
+
+        }
+        return en_buyuk;
+
+    }
+    public static int[] vektortoplamı(int[] vector1 , int[] vector2 ){
+        int[] vekt = new int[vector1.length];
+
+        if (vector1.length == vector2.length) {
+            for (int i = 0; i < vector1.length; i++) {
+                vekt[i] = vector1[i] + vector2[i];
+            }
+            return vekt;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public static int dotProduct (int[] vector1 , int[] vector2 ){
+        int dotProduct = 0;
+        if (vector1.length == vector2.length) {
+            for (int i = 0; i < vector1.length; i++) {
+                dotProduct += vector1[i] * vector2[i];
+
+            }
+            return dotProduct;
+        }
+        else {
+            return 0;
+        }
+    }
+
 
 }
